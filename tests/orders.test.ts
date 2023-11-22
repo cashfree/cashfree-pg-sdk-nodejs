f91108a662eeebece8ab462c293bafb77ffd92b8
Venkata Pranav <venkata.pranav@cashfree.com>
2023-11-22 16:28:53 +0530

    Removed Mocha and Added Jest

--- tests/orders.test.ts
+++ tests/orders.test.ts
@@ -1,5 +1,7 @@
 import * as assert from "assert";
-import { Cashfree } from "../api";
+import { Cashfree, CreateOrderRequest, UpiChannelEnum, OrderCreateRefundRequestRefundSpeedEnum } from "../api";
+const { execSync } = require('child_process');
+
 import { expect } from 'chai';
 
 Cashfree.XClientId = process.env.CLIENT_ID;
@@ -8,6 +10,7 @@ Cashfree.XEnvironment = Cashfree.Environment.SANDBOX;
 
 var orderId = ''
 var paymentSessionId = ''
+var successfulTxnId = ''
 
 
 function getCurrentTimeStamp() {
@@ -22,6 +25,58 @@ function getCurrentTimeStamp() {
 function setOrderDetails(order_id: string, payment_session_id: string) {
     orderId = order_id
     paymentSessionId = payment_session_id
+}
+
+function setSuccessfulTxnId(txnId: string) {
+    successfulTxnId = txnId
+}
+
+async function createOrder() {
+    var blah = 'foo';
+    var request = {
+        "order_amount": 3005,
+        "order_currency": "INR",
+        "customer_details": {
+            "customer_id": "iij",
+            "customer_name": "abcd",
+            "customer_email": "success@eligibility.com",
+            "customer_phone": "9999999999"
+        },
+
+        "order_note": "Test order",
+        "order_tags": {
+            "abc": "def"
+        }
+    }
+
+    await Cashfree.PGCreateOrder("2022-09-01", request).then((response) => {
+        setOrderDetails(response.data.order_id, response.data.payment_session_id)
+
+    }).catch((error) => {
+        console.log("Actual: " + error.actual + " Expected: " + error.expected + " Operator: " + error.operator)
+    });
+}
+
+async function createTxnWithStatus(payment_session_id: string, status: string) {
+    var upi_id = "testsuccess@gocash"
+    if (status === "failure") {
+        upi_id = "testfail@gocash"
+    }
+    var link_id = "Automated_Test_" + getCurrentTimeStamp()
+    // console.log("Payment SessionId: " + payment_session_id)
+    var request = {
+        "payment_session_id": payment_session_id,
+        "payment_method": {
+            "upi": {
+                "upi_id": upi_id,
+                "channel": UpiChannelEnum.COLLECT
+            }
+        }
+    }
+    await Cashfree.PGPayOrder("2022-09-01", request).then((response) => {
+    }).catch((error) => {
+        //console.log(error)
+    });
 }
 
 it('Create Order Test', function (done) {
@@ -41,6 +96,7 @@ it('Create Order Test', function (done) {
             "abc": "def"
         }
     }
+
     Cashfree.PGCreateOrder("2022-09-01", request).then((response) => {
         assert.equal(response.data.order_amount, request.order_amount)
         assert.equal(response.data.order_currency, "INR")
@@ -52,6 +108,13 @@ it('Create Order Test', function (done) {
         done()
     });
 
+});
+
+it('Order Pay Collect Test', function () {
+    createOrder();
+    execSync('sleep 1');
+    // console.log("Payment: " + paymentSessionId)
+    createTxnWithStatus(paymentSessionId, '')
 });
 
 it('Get Order Test', function (done) {
@@ -66,13 +129,10 @@ it('Get Order Test', function (done) {
 
 });
 
-
 it('Get Payments For an Order Test', function (done) {
+    createOrder()
+    createTxnWithStatus(paymentSessionId, '')
     Cashfree.PGOrderFetchPayments("2022-09-01", orderId).then((response) => {
-        // console.log(response.data.order_currency)
-        // assert.equal(response.data.order_currency, "INR")
-        // setOrderDetails(response.data.order_id)
-        // console.log(orderId)
         done()
     }).catch((error) => {
         console.log("Actual: " + error.actual + " Expected: " + error.expected + " Operator: " + error.operator)
@@ -114,9 +174,27 @@ it('Create Payment Link Test', function (done) {
     Cashfree.PGCreateLink("2022-09-01", request).then((response) => {
         done()
     }).catch((error) => {
+
         console.log("Actual: " + error.actual + " Expected: " + error.expected + " Operator: " + error.operator)
         assert.fail("Fail error message")
         done()
     });
 
+});
+
+
+it('Create Refund Test', function (done) {
+    var request = {
+        "refund_amount": 1,
+        "refund_id": "Refund_id_" + getCurrentTimeStamp(),
+        "refund_note": "initiated refund by automation"
+    }
+
+    Cashfree.PGOrderCreateRefund("2022-09-01", "order_4175412YWdHafgWZLFYcyTKkzIqu9QJvI", request).then((response) => {
+        done()
+    }).catch((error) => {
+        console.log(error)
+        assert.fail("Fail error message")
+        done()
+    });
 });
